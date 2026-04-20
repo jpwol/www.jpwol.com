4eb0db607901005c31b52be23cbaa3c9b9b96b65
jpwol <jpwolfe1981@gmail.com>
2026-04-20 05:35:37 -0400

    refactor(posts): updated post routing to allow for sorting

--- post-router.js
+++ post-router.js
@@ -1,23 +1,43 @@
-const fillPosts = () => {
-  fetch("/data/posts.json")
-    .then((res) => res.json())
-    .then((posts) => {
-      const main = document.querySelector("main");
-      main.innerHTML = posts
-        .map(
-          (post) => `
+let posts = [];
+
+fetch("/data/posts.json")
+  .then((res) => res.json())
+  .then((data) => {
+    posts = data;
+    fillPosts(posts);
+  });
+
+const formatDate = (dateStr) => {
+  const [year, month, day] = dateStr.split("-");
+  return `${month}/${day}/${year}`;
+};
+
+const fillPosts = (data) => {
+  const main = document.querySelector("main");
+  main.innerHTML = data
+    .map(
+      (post) => `
         <a class="post-listing" href="${post.url}">
           <img class="post-thumb" src="${post.thumb}">
-          <span class="post-date">${post.date}</span>
+          <span class="post-date">${formatDate(post.date)}</span>
           <div class="post-name">
             <span>${post.name}</span>
             <img src="${post.icon}" />
           </div>
         </a>
         `,
-        )
-        .join("");
-    });
+    )
+    .join("");
 };
 
-fillPosts();
+const sorts = {
+  nameAZ: (a, b) => a.name.localeCompare(b.name),
+  nameZA: (a, b) => b.name.localeCompare(a.name),
+  newest: (a, b) => new Date(b.date) - new Date(a.date),
+  oldest: (a, b) => new Date(a.date) - new Date(b.date),
+};
+
+const sortBy = (key) => {
+  const sorted = [...posts].sort(sorts[key]);
+  fillPosts(sorted);
+};

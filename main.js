073ac104fcd91043e9a234e5ba33614bb75f6baf
jpwol <jpwolfe1981@gmail.com>
2026-04-19 23:55:47 -0400

    refactor(components): more dynamic loading of html components

--- main.js
+++ main.js
@@ -4,13 +4,26 @@ const fillTail = () => {
   tail.textContent = "┃".repeat(Math.ceil(tail.offsetWidth / charWidth));
 };
 
+const injectHTML = (res, str) => {
+  let html = document.getElementById(str);
+  if (html != null) html.innerHTML = res;
+};
+
+const injectAll = (res, className) => {
+  document
+    .querySelectorAll(`.${className}`)
+    .forEach((el) => (el.innerHTML = res));
+};
+
 window.addEventListener("resize", fillTail);
 
 Promise.all([
   fetch("/components/nav.html").then((res) => res.text()),
   fetch("/components/footer.html").then((res) => res.text()),
-]).then(([nav, footer]) => {
-  document.getElementById("nav-placeholder").innerHTML = nav;
-  document.getElementById("footer-placeholder").innerHTML = footer;
+  fetch("/components/section-divider.html").then((res) => res.text()),
+]).then(([nav, footer, divider]) => {
+  injectHTML(nav, "nav-placeholder");
+  injectHTML(footer, "footer-placeholder");
+  injectAll(divider, "divider-placeholder");
   fillTail();
 });
